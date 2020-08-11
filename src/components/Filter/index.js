2f2a6d74dd9a6297311dd705dfa05235a6e9cb21
Bruno Braga Chagas <bbchagas@gmail.com>
2020-08-11 16:27:34 -0300

    Improve Filter layout

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -4,15 +4,29 @@ import PropTypes from "prop-types";
 
 const Wrapper = styled.div`
   padding: 6px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  max-width: 300px;
 `;
 
 const Label = styled.label`
   margin-right: 12px;
+  width: 40%;
 `;
 
 const Select = styled.select`
   padding: 6px 12px;
   background-color: transparent;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  width: 100%;
+  max-width: 230px;
+  min-width: 190px;
+  border-radius: 4px;
+  text-align-last: center;
 `;
 
 const Option = styled.option``;
