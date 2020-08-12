de3f69b52a0c7af05a1be19c53a2e114151cebce
Bruno Braga Chagas <bbchagas@gmail.com>
2020-08-11 21:08:04 -0300

    Add docs on code

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,3 +1,8 @@
+/**
+ * We need to have a default mock to get our tests running
+ * that involves axios running perfectly
+ */
+
 export default {
   get: jest.fn().mockResolvedValue({ data: {} }),
 };
